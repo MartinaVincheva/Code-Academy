0c73954e8026c0452e49ce00453172a73ec40005
Martina Vincheva <tufituri@gmail.com>
2021-01-18 16:02:52 +0200

    added 9 and 17 tasks

--- 20210115_Tasks/20210115_17.c
+++ 20210115_Tasks/20210115_17.c
@@ -1,2 +1,17 @@
-Задача 17.* Към задача 16 да се добави втора функция, която изчислява
-лицето на елипса по зададени два параметъра ( Пи * A * B)
+#include <stdio.h>
+
+const float PI = 3.14159;
+
+float areaEclipse(const float PI, float A, float B);
+
+int main() {
+	float A = 1;
+	float B = 2;
+	
+	printf("Area of Eclipse is %f\n", areaEclipse(PI, A, B));
+	
+	return 0;
+}
+	float areaEclipse(const float PI, float A, float B){
+		return PI * A * B;
+	}
