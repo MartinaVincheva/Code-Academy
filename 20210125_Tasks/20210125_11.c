a148d500c943c9302822755858ea73878d26bb46
Martina Vincheva <tufituri@gmail.com>
2021-01-26 19:30:07 +0200

    Homeword 25.01.2021

--- 20210125_Tasks/20210125_11.c
+++ 20210125_Tasks/20210125_11.c
@@ -1,23 +1,35 @@
 /* Задача 11.
 Напишете функция void reverse(char s[]), която обръща
 низа char s[]. Използвайте for цикъл с два аргумента , разделени със запетая
-i= 0, j = strlen (s) 1; i < j; i ++, j ----, за да обърнете стринга.*/
+i= 0, j = strlen (s) - 1; i < j; i ++, j ----, за да обърнете стринга.*/
 #include <stdio.h>
 #include <string.h>
 void reverse(char str[]) ;
 
-int main(void){
-    reverse("Kizomba") ;
+int main(void) {
+    char c, str[256];
+    int idx = 0;
+
+    while((c = getchar()) != EOF) {
+        if (c == '\n') {
+            break;
+        }
+        str[idx++] = c;
+    }
+    str[idx] = '\0';
+    printf("Non reversed string is '%s'\n", str);
+    reverse(str);
+    printf("Reversed string is '%s'\n", str);
     return 0;
 }
 
 void reverse(char str[]){
-    char rev [strlen(str)];
-    int i, j ;
+    int i, j;
+    char c;
    
-    for (i = 0 , j = strlen(str)-1 ; i<strlen(str); i++, j--){
-        rev[i] = str[j];
-        printf("%c", rev[i]);
+    for (i = 0 , j = strlen(str) - 1 ; i < j; i++, j--) {
+        c = str[i];
+        str[i] = str[j];
+        str[j] = c;
     }
-    printf("\n");
 }
