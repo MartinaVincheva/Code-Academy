f79f92bb5ae4d28b536d532aff2f00e419106942
Martina Vincheva <tufituri@gmail.com>
2021-01-31 12:06:20 +0200

    Homework 29.01.2021

--- 20210129_Tasks/20210129_11.c
+++ 20210129_Tasks/20210129_11.c
@@ -1,5 +1,68 @@
 /* Задача 11*. Напишете програма, която да премахва всички коментари в една С програма. */
 #include <stdio.h>
+#include <string.h>
+
+char program[] = "/* This is my test program */\n"
+"#include <stdio.h>\n"
+"\n"
+"/*\n"
+"This is my main function\n"
+"*/\n"
+"int main(void) {\n"
+"    /* For now we'll not add code here */\n"
+"    return(0);\n"
+"}\n"
+"\n"
+"/* This is the end of the program ! */\n";
+
+/* Returns index, where comment starts */
+int findCommentStart(char * str) {
+    char *tempStr = str;
+
+    while(*tempStr) {
+        if (*tempStr != '/') {
+            *tempStr++;
+        } else if (*(tempStr + 1) == '*') {
+            return (tempStr - str);
+        } else {
+            *tempStr++;
+        }
+    }
+    return (-1);
+}
+
+/* Returns index to the next char, after the comment */
+int findCommentEnd(char * str) {
+    char *tempStr = str;
+
+    while(*tempStr) {
+        if (*tempStr != '*') {
+            *tempStr++;
+        } else if (*(tempStr + 1) == '/') {
+            return ((tempStr + 2) - str);
+        } else {
+            *tempStr++;
+        }
+    }
+    return (-1);
+}
+
+/* Prints 'count' characters from string */
+void printTillIndex(char *str, int count) {
+    while (count) {
+        printf("%c", *str++);
+        count--;
+    }
+}
 
 int main(void) {
+    char *str = program;
+    int last_idx = 0, idx;
 
+    while ((idx = findCommentStart(str)) >= 0) {
+        /* Print program till start of comment */
+        printTillIndex(str, idx);
+        str += findCommentEnd(str);
+    }
+    return 0;
+}
