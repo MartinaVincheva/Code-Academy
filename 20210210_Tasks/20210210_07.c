c25c959d0fb9030709601319c1822d81b660690a
Martina Vincheva <tufituri@gmail.com>
2021-02-11 18:13:53 +0200

    Homework 10.02.2021

--- 20210210_Tasks/20210210_07.c
+++ 20210210_Tasks/20210210_07.c
@@ -2,39 +2,55 @@
 #include <stdio.h>
 
 void quicksort(int a[], int l, int r);
-void swap(int a[], int i, int j);
+void swap(int *a, int *b);
+int partition(int a[], int l, int r);
 
-int main(){
-    int ArrInt[] = { 100, 2, 30, 4, 50, 6};
-    
+int main()
+{
+    int ArrInt[] = {100, 2, 30, 4, 50, 6};
+    int size = sizeof(ArrInt) / sizeof(ArrInt[0]);
+    int i;
 
-    quicksort(ArrInt,6,6);
+    quicksort(ArrInt, 0, size - 1);
+    for (i = 0; i < size; i++)
+        printf("%d ", ArrInt[i]);
+
+    printf("\n");
     return 0;
 }
 
-void quicksort(int a[], int l, int r){
-    int i, last;
-    void swap(int a[], int i, int j);
-    
-    if (l >= r){
-        return;
+void quicksort(int a[], int l, int r)
+{
+    if (l <= r)
+    {
+        int i = partition(a, l, r);
+
+        quicksort(a, l, i - 1);
+        quicksort(a, i + 1, r);
     }
-    swap(a, l, (l + r) / 2);
-    last = l;
-    for (i = l + 1; i <= r; i++){
-        if (a[i] < a[l]){
-            swap(a, ++last, i);
+}
+void swap(int *a, int *b)
+{
+    int temp;
+
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int partition(int a[], int l, int r)
+{
+    int pivot = a[r];
+    int i = (l - 1);
+
+    for (int j = l; j <= r - 1; j++)
+    {
+        if (a[j] < pivot)
+        {
+            i++;
+            swap(&a[i], &a[j]);
         }
     }
-    swap(a, l, last);
-    quicksort(a, l, last - 1);
-    quicksort(a, last + 1, r);
+    swap(&a[i + 1], &a[r]);
+    return (i + 1);
 }
-
-void swap(int a[], int i, int j){
-    int temp;
-    temp = a[i];
-    a[i] = a[j];
-    a[j] = temp;
-}
-
