27f4a483da00422060831f58f8268483af2efa59
Martina Vincheva <tufituri@gmail.com>
2021-02-18 18:07:05 +0200

    Homework 17.02.2021

--- 20210217_Tasks/20210217_15.c
+++ 20210217_Tasks/20210217_15.c
@@ -7,9 +7,54 @@
 което не е извадено до момента.
 3.Програмата вади числата, нужни за избраната игра (5 или 6).*/
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+void totalizator(int choice)
+{
+    int n;
+    if (choice == 1)
+    {
+        int min = 5;
+        int max = 35;
+        for (int i = 0; i < min; i++)
+        {
+            n = (rand() % (max - min + 1)) + min;
+            printf("Your random numbers are : %d ", n);
+        }
+        printf("\n");
+    }
+    if (choice == 2)
+    {
+        int min = 6;
+        int max = 42;
+        for (int i = 0; i < min; i++)
+        {
+            n = (rand() % (max - min + 1)) + min;
+            printf("Your random numbers are : %d ", n);
+        }
+        printf("\n");
+    }
+    if (choice == 3)
+    {
+        int min = 6;
+        int max = 49;
+        for (int i = 0; i < min; i++)
+        {
+            n = (rand() % (max - min + 1)) + min;
+            printf("Your random numbers are : %d ", n);
+        }
+        printf("\n");
+    }
+}
 
 int main(void)
 {
-
+    int choice;
+    srand(time(NULL));
+    void (*toto)(int) = &totalizator;
+    printf("Welcome!\nPlease chose which game you want to play: \n1 - (5 of 35)\n2 - (6 of 42)\n3 - (6 of 49)\nenter your choice: ");
+    scanf("%d", &choice);
+    toto(choice);
     return 0;
 }
