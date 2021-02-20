6672281354ecc543b978c04a0b99003b98d84fc9
Martina Vincheva <tufituri@gmail.com>
2021-02-20 17:28:13 +0200

    Homework 19.02.2021

--- 20210219_Tasks/20210219_09.c
+++ 20210219_Tasks/20210219_09.c
@@ -1,9 +1,20 @@
 /*Задача 9. Направете функця add(), която събира две точки.*/
-
 #include <stdio.h>
+struct point
+{
+    int x;
+    int y;
+};
 
+void addPoints(struct point *a, struct point *b, struct point *c)
+{
+    c->x = a->x + b->x;
+    c->y = a->y + b->y;
+}
 int main()
 {
-
+    struct point a = {0, 5}, b = {3, 2}, c;
+    addPoints(&a, &b, &c);
+    printf("Resulting point is %d,%d\n", c.x, c.y);
     return 0;
 }
