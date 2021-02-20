11f108cf567bbc3168759ec4727361741f36c89f
Martina Vincheva <tufituri@gmail.com>
2021-02-20 19:01:54 +0200

    Homework 19.02.2021

--- 20210219_Tasks/20210219_08.c
+++ 20210219_Tasks/20210219_08.c
@@ -2,11 +2,39 @@
 точки. Създайте обект от тип тази структура наречен screen. Използвайте
 функцията makepoint за да зададете начална точка на екрана (0, 0) и
 крайна точка (15, 15). Запълнете пространството между тях с тирета.*/
-
 #include <stdio.h>
 
-int main()
+struct point
 {
+    int x;
+    int y;
+};
+struct rect
+{
+    struct point one;
+    struct point two;
+} screen;
 
+struct point makePoint(int x, int y)
+{
+    struct point NewPoint = {x, y};
+    return NewPoint;
+}
+
+int main(void)
+{
+    int i, j;
+    struct point a = makePoint(0, 0);
+    struct point b = makePoint(15, 15);
+    struct rect screen = {a, b};
+
+    for (int i = a.y; i < b.y; i++)
+    {
+        for (int j = a.x; j < b.x; j++)
+        {
+            printf("- ");
+        }
+        printf("\n");
+    }
     return 0;
 }
