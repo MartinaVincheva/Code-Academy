2b5f05b695c3ee32ce6411f564aebf51c32f747d
Martina Vincheva <tufituri@gmail.com>
2021-02-20 17:40:56 +0200

    Homework 19.02.2021

--- 20210219_Tasks/20210219_07.c
+++ 20210219_Tasks/20210219_07.c
@@ -3,8 +3,22 @@
 
 #include <stdio.h>
 
+struct point
+{
+    int x;
+    int y;
+} a, b;
+struct point makePoint(int x, int y)
+{
+    struct point NewPoint = {x, y};
+    return NewPoint;
+}
 int main()
 {
+    struct point a = makePoint(1, 2);
+    struct point b = makePoint(3, 4);
 
+    printf("Point a %d, %d\n", a.x, a.y);
+    printf("Point b %d, %d\n", b.x, b.y);
     return 0;
 }
