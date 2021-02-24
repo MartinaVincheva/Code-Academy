70d671ea76c489cd97960fbce6b5077137bcac70
Martina Vincheva <tufituri@gmail.com>
2021-02-24 09:36:51 +0200

    Homework 22.02.2021

--- 20210222_Task/20210222_14.c
+++ 20210222_Task/20210222_14.c
@@ -5,13 +5,35 @@
 
 struct date
 {
-    unsigned month m:1;
-    unsigned day d:1;
-    unsigned year y:1;
+    short int month : 8;
+    short int day : 8;
+    short int year : 16;
 };
+
+int ValidateDate(struct date *p_Struct)
+{
+    if ((p_Struct->day < 1 || p_Struct->day > 31) || (p_Struct->month < 1 || p_Struct->month > 12) || (p_Struct->year < 1))
+        return 0;
+    else
+        return 1;
+}
+
+void my_print(struct date *p_Struct)
+{
+    printf("Date is : %d.%d.%d .\n", p_Struct->day, p_Struct->month, p_Struct->year);
+}
 
 int main()
 {
+    struct date FirstDate = {23, 2, 2021};
+
+    if (ValidateDate(&FirstDate) != '0')
+    {
+        printf("Validate ");
+        my_print(&FirstDate);
+    }
+    else
+        printf("Error!\n");
 
     return 0;
 }
