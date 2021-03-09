b49097a49c9264df7a1e6fda2a4fa765fc40df11
Martina Vincheva <tufituri@gmail.com>
2021-03-09 17:35:34 +0200

    Homework 08.03.2021

--- 20210308_Tasks/20210308_14.c
+++ 20210308_Tasks/20210308_14.c
@@ -5,9 +5,40 @@
 име. Отпечатайте съдържанието на файл на
 стандартния изход с главни букви.*/
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
+char atoA(char c)
 {
+    if (c >= 'a' && c <= 'z')
+    {
+        c = c - 32;
+    }
+    return c;
+}
 
+int main(int argc, char *argv[])
+{
+    FILE *fpIn = NULL;
+    FILE *fpOut = NULL;
+    char c;
+    fpIn = fopen("C:\\Users\\Marti\\codeAcademy\\gitRepo\\Code-Academy\\20210308_Tasks\\file14.txt", "r");
+    fpOut = fopen("C:\\Users\\Marti\\codeAcademy\\gitRepo\\Code-Academy\\20210308_Tasks\\outfile14.txt", "w");
+    if (NULL != fpIn && NULL != fpOut)
+    {
+        while ((c = getc(fpIn)) != EOF)
+        {
+            c = atoA(c);
+            fprintf(fpOut, "%c", c);
+        }
+    }
+    else
+    {
+        printf("Error to open file\n");
+    }
+
+    fclose(fpIn);
+    fclose(fpOut);
+    remove("C:\\Users\\Marti\\codeAcademy\\gitRepo\\Code-Academy\\20210308_Tasks\\file14.txt");
+    rename("C:\\Users\\Marti\\codeAcademy\\gitRepo\\Code-Academy\\20210308_Tasks\\outfile14.txt", "C:\\Users\\Marti\\codeAcademy\\gitRepo\\Code-Academy\\20210308_Tasks\\file14.txt");
     return 0;
 }
