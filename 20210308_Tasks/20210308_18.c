7e07e10a0f9dd4a5ee464eb4dec6fd3fe3691a38
Martina Vincheva <tufituri@gmail.com>
2021-03-09 19:55:11 +0200

    Homework 08.03.2021

--- 20210308_Tasks/20210308_18.c
+++ 20210308_Tasks/20210308_18.c
@@ -7,8 +7,31 @@ char gender;
 в XML формат по показания в лекцията начин.*/
 #include <stdio.h>
 
-int main()
-{
+static const char *FORMAT_PERSON_IN = "(%[^,], %d, %c)\n";
+static const char *FORMAT_PERSON_OUT = "(%s, %d, %c)\n";
 
+typedef struct Person
+{
+    char name[20];
+    int age;
+    char gender;
+} t_person;
+
+int main(void)
+{
+    t_person m = {
+        .name = "Martina",
+        .age = 33,
+        .gender = 'F'};
+    t_person dm;
+    FILE *fp;
+    fp = fopen("C:\\Users\\Marti\\codeAcademy\\gitRepo\\Code-Academy\\20210308_Tasks\\people.dat", "w+");
+    if (NULL == fp)
+        return 1;
+    fprintf(fp, FORMAT_PERSON_OUT, m.name, m.age, m.gender);
+    fseek(fp, 0, SEEK_SET);
+    fscanf(fp, FORMAT_PERSON_IN, dm.name, &dm.age, &dm.gender);
+    printf(FORMAT_PERSON_OUT, dm.name, dm.age, dm.gender);
+    fclose(fp);
     return 0;
 }
