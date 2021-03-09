3320ca9d8a0e87f93a39a8ea4d6f38ad4c82ee9b
Martina Vincheva <tufituri@gmail.com>
2021-03-09 22:25:56 +0200

    Homework 08.03.2021

--- 20210308_Tasks/20210308_19.c
+++ 20210308_Tasks/20210308_19.c
@@ -4,9 +4,36 @@
 аргументи, то тя да изкара съдържанието подадено от стандартния вход
 на стандартния изход.*/
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
+int main(int argc, char *argv[])
 {
+    FILE *fpt = NULL;
 
+    if (argc < 2)
+    {
+        printf("\nUsage:\n > %s file19.txt\n", argv[0]);
+    }
+    else
+    {
+        fpt = fopen(argv[1], "r");
+    }
+
+    if (fpt != NULL)
+    {
+        char c;
+
+        while (1)
+        {
+            c = fgetc(fpt);
+            printf("%c", c);
+
+            if (feof(fpt))
+                break;
+        }
+    }
+
+    fclose(fpt);
+    fpt = NULL;
     return 0;
 }
