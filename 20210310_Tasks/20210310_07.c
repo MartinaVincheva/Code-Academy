1b631d5d7b4e75bbaa46baf5660c273dee91681d
Martina Vincheva <tufituri@gmail.com>
2021-03-11 16:35:51 +0200

    Homework 10.03.2021

--- 20210310_Tasks/20210310_07.c
+++ 20210310_Tasks/20210310_07.c
@@ -1,15 +1,34 @@
 /* Задача 7 . Напишете програма, която обработва Ctrl+C
 Ctrl+D, Ctrl+Z  */
-#include <stdio.h>
+#include <signal.h>
 #include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
 
-char *getLinefromFile(FILE *f)
+void sleep(int);
+
+void ctrlc_handler(int iUnused)
 {
+    printf("\nCtrl + C\n");
+    exit(EXIT_FAILURE);
+}
+void ctrld_handler(int iUnused)
+{
+    printf("\nCtrl + D\n");
+    exit(EXIT_FAILURE);
+}
+void ctrlz_handler(int iUnused)
+{
+    printf("\nCtrl + Z\n");
+    exit(EXIT_FAILURE);
 }
 
-int main()
+int main(void)
 {
+    signal(SIGINT, ctrlc_handler);
+    signal(SIGINT, ctrld_handler);
+    signal(SIGINT, ctrlz_handler);
 
+    while (!0)
+        sleep(1);
     return 0;
 }
