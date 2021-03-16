85feac122168d366e1400e74059e32df2570a950
Martina Vincheva <tufituri@gmail.com>
2021-03-16 10:26:24 +0200

    Homework 15.03.2021

--- 20210315_Tasks/20210315_03/main.c
+++ 20210315_Tasks/20210315_03/main.c
@@ -15,7 +15,6 @@
 
 treeNode_t *tree = NULL;
 
-
 int main()
 {
     int i;
@@ -24,16 +23,58 @@ int main()
     for (i = 0; i < SIZE; i++)
         insert(arr[i]);
 
-    printf("\n%d\n", tree->data);
+    printf("\nTop is %d\n", tree->data);
     preorder(tree);
     printf("\n");
-    printf("Right part:");
-    printRight(tree);
-    printf("\nLeft part:");
-    printLeft(tree);
-    printf("\n");
-    
-
     deltree(tree);
+    tree = NULL;
+
+    int arr1[SIZE] = {12, 7, 14, 81, 42, 18, 61, 4, 64, 35, 13, 28, 65};
+
+    for (i = 0; i < SIZE; i++)
+        insert(arr1[i]);
+
+    printf("\nTop is %d\n", tree->data);
+    preorder(tree);
+    printf("\n");
+    deltree(tree);
+    tree = NULL;
+
+    int arr2[SIZE] = {4, 7, 12, 13, 14, 18, 28, 35, 42, 61, 64, 65, 81};
+
+    for (i = 0; i < SIZE; i++)
+        insert(arr2[i]);
+
+    printf("\nTop is %d\n", tree->data);
+    preorder(tree);
+    printf("\n");
+    deltree(tree);
+    tree = NULL;
+
+    int arr3[SIZE] = {81, 65, 64, 61, 42, 35, 28, 18, 14, 13, 12, 7, 4};
+
+    for (i = 0; i < SIZE; i++)
+        insert(arr3[i]);
+
+    printf("\nTop is %d\n", tree->data);
+    preorder(tree);
+    printf("\n");
+    deltree(tree);
+    tree = NULL;
+
+    int arr4[SIZE] = {28, 64, 13, 42, 7, 81, 61, 4, 12, 65, 35, 18, 14};
+
+    for (i = 0; i < SIZE; i++)
+        insert(arr4[i]);
+
+    printf("\nTop is %d\n", tree->data);
+    preorder(tree);
+    printf("\n");
+    deltree(tree);
+    tree = NULL;
+
     return 0;
 }
+
+/*При едни и същи числа , но с различен връх и различна подредба се получават различни дървета. 
+Например подусловие д) е най-балансираното дърво. Подусловия в) е изцяло дясно ориентирано, подгусловие г) е изцяло ляво ориентирано.*/
